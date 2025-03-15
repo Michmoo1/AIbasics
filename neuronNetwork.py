c803746d97b7a870ec76338621e953b661baad6d
Michal Morawiec <yMichaelmorawiec@wp.pl>
2025-03-15 22:37:24 +0100

    Definition and traing of NN

--- neuronNetwork.py
+++ neuronNetwork.py
@@ -7,3 +7,62 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 
 
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+])
+
+#Datasets
+trainset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+testset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+
+# Loaders
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
+
+examples = iter(trainloader)
+images, labels = next(examples)
+
+# Example
+fig, axes = plt.subplots(1, 6, figsize=(10,3))
+for i in range(6):
+    axes[i].imshow(images[i].squeeze(), cmap='gray')
+    axes[i].axis('off')
+plt.show()
+
+# Model definition
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.fc1 = nn.Linear(28*28, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 28*28)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+model = NeuralNetwork()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+epochs = 5
+
+for epoch in range(epochs):
+    running_loss = 0.0
+
+    for images, labels in trainloader:
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    print(f"Epoch {epoch+1}, Loss: {running_loss/len(trainloader):.4f}")
+
+print("Training ended.")
+
+# Evaluation of model
